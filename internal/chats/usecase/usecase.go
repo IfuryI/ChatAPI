fa34e7fbdbe1b7f6e819e8103ca3eb2eab933481
Ilya Afimin <fur1ed@icloud.com>
2021-08-02 14:17:13 +0300

    Изменено: фикс файлов

--- internal/chats/usecase/usecase.go
+++ internal/chats/usecase/usecase.go
@@ -8,13 +8,13 @@ import (
 	"github.com/IfuryI/ChatAPI/internal/users"
 )
 
-// ChatUseCase структура usecase рецензий
+// ChatUseCase структура usecase чата
 type ChatUseCase struct {
 	chatRepository chats.Repository
 	userRepository users.UserRepository
 }
 
-// NewChatUseCase инициализация структуры usecase рецензий
+// NewChatUseCase инициализация структуры usecase чата
 func NewChatUseCase(chatRepo chats.Repository, userRepo users.UserRepository) *ChatUseCase {
 	return &ChatUseCase{
 		chatRepository: chatRepo,
@@ -22,7 +22,7 @@ func NewChatUseCase(chatRepo chats.Repository, userRepo users.UserRepository) *C
 	}
 }
 
-// CreateChat создание рецензии
+// CreateChat создание чата
 func (chatUC *ChatUseCase) CreateChat(chat *models.Chat) (int, error) {
 	_, err := chatUC.chatRepository.GetChatByName(chat.Name)
 	if err == nil {
@@ -32,7 +32,7 @@ func (chatUC *ChatUseCase) CreateChat(chat *models.Chat) (int, error) {
 	return chatUC.chatRepository.CreateChat(chat)
 }
 
-// AddMessageToChat создание рецензии
+// AddMessageToChat создание сообщения в чате
 func (chatUC *ChatUseCase) AddMessageToChat(message *models.Message) (int, error) {
 	err := chatUC.chatRepository.IsUserInChat(message.UserID, message.ChatID)
 	if err != nil {
@@ -42,7 +42,7 @@ func (chatUC *ChatUseCase) AddMessageToChat(message *models.Message) (int, error
 	return chatUC.chatRepository.AddMessageToChat(message)
 }
 
-// GetAllUserChats создание рецензии
+// GetAllUserChats получение всех чатов пользователя
 func (chatUC *ChatUseCase) GetAllUserChats(userID string) ([]*models.Chat, error) {
 	_, err := chatUC.userRepository.GetUserByID(userID)
 	if err != nil {
@@ -52,7 +52,7 @@ func (chatUC *ChatUseCase) GetAllUserChats(userID string) ([]*models.Chat, error
 	return chatUC.chatRepository.GetAllUserChats(userID)
 }
 
-// GetAllChatMessages создание рецензии
+// GetAllChatMessages получение всех сообщений чата
 func (chatUC *ChatUseCase) GetAllChatMessages(chatID string) ([]*models.Message, error) {
 	_, err := chatUC.chatRepository.GetChatByID(chatID)
 	if err != nil {
